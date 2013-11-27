80efe7981cf13490c8ac1ffc6be68a1062269b9f
Marten Lienen <marten.lienen@gmail.com>
2013-11-27 17:47:52 +0100

    Implement a rails template handler

--- lib/hash_builder/template.rb
+++ lib/hash_builder/template.rb
@@ -1,16 +1,28 @@
-if defined?(Rails)
-  class ActionView::Template::Handlers::JsonBuilder
-    class_attribute :default_format
-    self.default_format = Mime::JSON
-
-    def self.call (template)
-      <<-RUBY
-(HashBuilder.build_with_env(scope: self, locals: local_assigns) do
-  #{template.source}
-end).to_json
-RUBY
+module HashBuilder
+  class Template
+    def self.default_format
+      Mime::JSON
     end
 
-    ActionView::Template.register_template_handler :json_builder, ActionView::Template::Handlers::JsonBuilder
+    def self.call (template)
+      render_code = <<-RUBY
+(HashBuilder.build_with_env(scope: self, locals: local_assigns) do
+  #{template.source}
+end)
+RUBY
+      if !is_partial?(template)
+        render_code += ".to_json"
+      end
+      
+      render_code
+    end
+
+    def self.is_partial? (template)
+      template.virtual_path.split("/").last.start_with?("_")
+    end
   end
+end
+
+if defined?(Rails)
+  ActionView::Template.register_template_handler :json_builder, HashBuilder::Template
 end
